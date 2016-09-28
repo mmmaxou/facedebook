3f53a6ad027573f64bc101f314abaf2bf8a75cb9
Pluchard Maximilien <mmmaxou@yahoo.fr>
2016-09-28 11:06:57 +0200

    Demande d'amitié non testée

--- traitement/demanderamitie.php
+++ traitement/demanderamitie.php
@@ -4,7 +4,7 @@ include("../divers/connexion.php");
 include("../divers/balises.php");
 
 if(!isset($_SESSION['id'])) {
-	// On n'est pas connecté, il faut retourner à la pgae de login
+	// On n'est pas connecté, il faut retourner à la page de login
 	header("Location:login.php");
 }
 
@@ -16,14 +16,22 @@ if(!isset($_SESSION['id'])) {
 // Le quatrième paramètre $_SESSION['id']
 // Si il y a une réponse il n'y a rien à faire.
 
+$sql = "SELECT * FROM lien WHERE (idUtilisateur1=? AND idUtilisateur2=?) OR  (idUtilisateur1=? AND idUtilisateur2=?)";
+$query = $pdo->prepare($sql);
+$query->execute( array($_SESSION['id'], $_POST['id'], $_POST['id'], $_SESSION['id']) );
+$line = $query->fetch();
 
-// La requete de demande de creation amitie : INSERT INTO lien VALUES(?,?,"attente");
-// Le premier parametre de la requête est le SESSION['id'] : celui qui a demandé l'amitié
-// Le second parametre de la requete est le $_POST['id'] 
-
-
-
-
+if ( $line != false ) {
+    
+    // La requete de demande de creation amitie : INSERT INTO lien VALUES(?,?,"attente");
+    // Le premier parametre de la requête est le SESSION['id'] : celui qui a demandé l'amitié
+    // Le second parametre de la requete est le $_POST['id'] 
+    
+    $sql = "INSERT INTO lien VALUES(?,?,'attente')";
+    $query = $pdo->prepare($sql);
+    $query->execute( array($_SESSION['id'], $_POST['id']) );
+    
+}
 
 // A la fin on retourne à la page d'amitié : 
 header("Location:affichage/ami.php");

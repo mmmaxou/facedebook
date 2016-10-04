6effe4969af7d490117ad089f6172bb72b31c51f
Draelnir <g.lollier@hotmail.fr>
2016-10-04 12:39:19 +0200

    Ecrire fini mais bug sur le mur

--- traitement/ecrire.php
+++ traitement/ecrire.php
@@ -12,7 +12,11 @@ if(!isset($_SESSION['id'])) {
 
 // Ecrire un message
 if(isset($_POST['statut'])){
-    $sql = "INSERT INTO ecrit FROM ";
+    $sql = "INSERT INTO ecrit VALUES(NULL,?,?,?,NULL,?,?)";
+    $query =$pdo->prepare($sql);
+    $query -> execute(array($_POST['titre'],$_POST['statut'],date("Y-m-d h:i:s"),$_SESSION['id'],$_POST['id']));
+    
+    header("Location:../affichage/mur.php?id=".$_POST['id']);
 }
 
 

8c87690158deafc297faa7aafd50d85ddf92ea3c
Pluchard Maximilien <mmmaxou@yahoo.fr>
2016-10-04 12:11:40 +0200

    reset bootstrap

--- affichage/entete.php
+++ affichage/entete.php
@@ -1,9 +1,14 @@
 <!doctype html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>Titre de la page</title>
-  <link rel="stylesheet" href="../css/style.css">
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/facebook-styles.css">
+    <link rel="stylesheet" href="../css/style.css">
+    
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/scripts.js"></script>
 </head>
 
 

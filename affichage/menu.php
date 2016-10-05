96ef6ad4f8106c17d343365e739f615218980779
mmmaxou <mmmaxou@yahoo.fr>
2016-10-05 23:32:58 +0200

    Affichage du mur

--- affichage/menu.php
+++ affichage/menu.php
@@ -13,46 +13,48 @@ $query->execute( array( $_SESSION['id'] ) );
 $line = $query->fetch();
 
 
-echo "<div class='menu'><ul>";
-echo "<li>".lien("mur.php?id=".$_SESSION['id'],"Bonjour ".$line['login'])."</li>";
-echo item(lien("ami.php", "Mes amis"));
+echo "<div class='menu'><div class='wrapper-menu'><ul>";
+echo item(lien("mur.php?id=".$_SESSION['id'],"Bonjour ".$line['login']), array('class'=>'link'));
+echo item(lien("ami.php", "Mes amis"), array('class'=>'link'));
 
 ?>
 
 
-    
-    <li>
-        <form method="post" action="#">
-            Rechercher un ami
-            <input type="text" name="search" placeholder="Son pseudo" required>
-            <input type="submit" value="valider">
-        </form>
-    </li>
-    <li>
-        <form method="get" action="../traitement/deconnexion.php">
-            <input type="submit" name="action" value="deconnexion"> 
-        </form>
-    </li>
+
+<li>
+    <form method="post" action="#">
+        <div class="search">
+            <input type="text" name="search" id="search_text" placeholder="Chercher un ami" required>
+            <input type="submit" id="search_button" value="valider">
+        </div>
+    </form>
+</li>
+<li>
+    <form method="get" action="../traitement/deconnexion.php">
+        <input type="submit" id="deconnexion" name="action" value="deconnexion"> 
+    </form>
+</li>
 </ul>
+</div>
 </div>
 <div class="container">
 
 
-<?php
+    <?php
 
-//Requete de recherche des amis
-if ( isset($_POST['search']) ) {
-    
-    $search = $_POST['search'];
-    $sql = "SELECT * FROM utilisateur WHERE login LIKE '%".$search."%'";
-    $query = $pdo->prepare($sql);
-    $query -> execute();
-    
-    while ( $line = $query->fetch() ) {
-        
-        echo lien("mur.php?id=".$line['id'], $line['login']);
-        
+    //Requete de recherche des amis
+    if ( isset($_POST['search']) ) {
+
+        $search = $_POST['search'];
+        $sql = "SELECT * FROM utilisateur WHERE login LIKE '%".$search."%'";
+        $query = $pdo->prepare($sql);
+        $query -> execute();
+
+        while ( $line = $query->fetch() ) {
+
+            echo lien("mur.php?id=".$line['id'], $line['login']);
+
+        }
     }
-}
 
-?>
+    ?>
--- affichage/mur.php
+++ affichage/mur.php
@@ -4,16 +4,16 @@ include("../divers/connexion.php");
 include("../divers/balises.php");
 
 if(!isset($_SESSION['id'])) {
-	// On n'est pas connecté, il faut retourner à la pgae de login
-	header("Location:login.php");
+    // On n'est pas connecté, il faut retourner à la pgae de login
+    header("Location:login.php");
 }
 
 include("entete.php");
 
 if(!isset($_GET['id']) || !is_numeric($_GET['id'])) {
-	// On n a pas donné le numéro de l'id de la personne dont on veut afficher le mur.
-	// On affiche un message et on meurt
-	echo "Bizarre !!!!";die(1);
+    // On n a pas donné le numéro de l'id de la personne dont on veut afficher le mur.
+    // On affiche un message et on meurt
+    echo "Bizarre !!!!";die(1);
 }
 
 // On affiche le menu
@@ -32,20 +32,20 @@ echo "<h1>Mur de ".$line['login']."</h1>";
 $ok = false;
 $etat = null;
 if($_GET['id']==$_SESSION['id']) {
-	$ok = true; // C notre mur, pas de soucis
+    $ok = true; // C notre mur, pas de soucis
 } else {
-	// Verifions si on est amis avec cette personne
-	$sql = "SELECT * FROM lien WHERE etat='ami' 
+    // Verifions si on est amis avec cette personne
+    $sql = "SELECT * FROM lien WHERE etat='ami' 
 		AND ((idUtilisateur1=? AND idUtilisateur2=?) OR (idUtilisateur1=? AND idUtilisateur2=?))";
 
-	// les deux ids à tester sont : $_GET['id'] et $_SESSION['id']		
-	// A completer. Il faut récupérer une ligne, si il y en a pas ca veut dire que l'on est pas ami avec cette personne
-    
+    // les deux ids à tester sont : $_GET['id'] et $_SESSION['id']		
+    // A completer. Il faut récupérer une ligne, si il y en a pas ca veut dire que l'on est pas ami avec cette personne
+
     $query=$pdo->prepare($sql);
     $query->execute( array($_GET['id'], $_SESSION['id'], $_SESSION['id'], $_GET['id']));
-    
+
     $line = $query->fetch();
-    
+
     if ( $line['etat'] != "ami" ) {
         $ok = false;
     } else {
@@ -55,24 +55,24 @@ if($_GET['id']==$_SESSION['id']) {
 
 }
 if($ok == false) {
-	echo "Vous n'êtes pas encore ami, vous ne pouvez voir son mur !!<br/>";
-    
+    echo "Vous n'êtes pas encore ami, vous ne pouvez voir son mur !!<br/>";
+
     $sql = "SELECT * FROM lien WHERE (idUtilisateur1=? AND idUtilisateur2=?) OR (idUtilisateur1=? AND idUtilisateur2=?)";
     $query = $pdo->prepare($sql);
     $query -> execute(array($_GET['id'], $_SESSION['id'], $_SESSION['id'], $_GET['id']));
-    
+
     $line = $query->fetch();
     $etat = $line['etat'];
-    
-    
+
+
     if ($etat == "attente") {
         echo "Invitation deja envoyée";
     } else if ($etat == "banni") {
         echo "L'utilisateur vous a banni";
     } else {
-    echo "<a href='../traitement/demanderamitie.php?id=".$_GET['id']."'><input type='button' value='Demander en Ami'></a>";
+        echo "<a href='../traitement/demanderamitie.php?id=".$_GET['id']."'><input type='button' value='Demander en Ami'></a>";
     }	
-    
+
     die(1);
 }
 
@@ -83,69 +83,80 @@ if($ok == false) {
 // SELECT * FROM ecrit WHERE idAmi=?
 // le paramètre  est le $_GET['id']
 
+//Formulaire de post
+echo "<div class='message'>";
+
+echo "<form action='../traitement/ecrire.php' method='POST' enctype='multipart/form-data'>";
+echo "<form action='../traitement/ecrire.php' method='POST'>
+
+        <p>Poster un message : </p>
+        <input type='text' name='titre' value='Titre'>
+        <textarea name='statut' placeholder='Votre message ...' ></textarea><br/>
+        <input type='submit' value='Poster'>
+        <input type='hidden' name='id' value=".$_GET['id'].">
+      </form>";
+echo "<form action='../traitement/uploader.php' method='POST' enctype='multipart/form-data'>
+        <input type='file' id='hiddenfile' name='imageStatut' style='display:none' onchange='getfile()'>
+        <input type='button' value='Ajouter une image' onclick='getfile()' />
+        
+        <p id='demo'></p>
+      </form>
+    ";
+echo "</div>";
+
+
+//Affichage des post
+echo "<div class='ecrit'>";
+
 $sql = "SELECT * FROM ecrit WHERE idAmi=?";
 $query=$pdo->prepare($sql);
 $query->execute(array($_GET['id']));
 
 while ($line = $query-> fetch() ) {
-    
-//    renderArray($line);
-    
+
+    //    renderArray($line);
+
     $sql = "SELECT * FROM utilisateur WHERE id=?";
     $q = $pdo->prepare($sql);
     $q->execute(array($line["idAuteur"]));
-    
-    
+
+
     while ( $auteur = $q->fetch()) {
-                
+
         afficherPost( $line, $auteur );
-        
+
     } 
-    
+
 }
 
 function afficherPost( $data, $auteur) {
     echo "<div class='well'>";
-    
-    echo $data["titre"]."<br/>";
-    echo $data["contenu"]."<br/>";
-    echo $data["dateEcrit"]."<br/>";
-    echo "auteur : ".$auteur["login"]."<br/>";
-    
+
+    echo "<h3>".$data["titre"]."</h3><br/>";
+    echo "<p class='texte'>".$data["contenu"]."</p>";
+    echo "<div class='hr'><hr /></div>";
+    echo "<p class='sous-texte'>Ecrit le ";
+    echo $data["dateEcrit"];
+    echo " par ".$auteur["login"];
+
+    echo lien("../traitement/effacer.php?id=".$data['id'],"X | Supprimer");
+
+    echo "</p>";
+
     if (isset($data['image'])) {
         echo "il y a une image";
     } else {
         echo "il n'y a pas d'image";
     }
-    
-    echo lien("../traitement/effacer.php?id=".$data['id']," Supprimer");
+
 
 
     echo "</div>";
 
-    
+
 }
 
-
-echo "<form action='../traitement/ecrire.php' method='POST' enctype='multipart/form-data'>";
-
-echo "<form action='../traitement/ecrire.php' method='POST'>
-
-        Entre ton titre enculé : <input type='text' name='titre' value='titre'>
-        <textarea name='statut' style='resize:none' ></textarea><br/>
-        <input type='submit' value='Poster'>
-        <input type='hidden' name='id' value=".$_GET['id'].">
-      </form>
-    ";
-
-echo "<form action='../traitement/uploader.php' method='POST' enctype='multipart/form-data'>
-        <input type='file' name='imageStatut'>
-        <input type='text' name='descriptionImageStatut'>
-      </form>
-    ";
-
-
-
+echo '</div>';
 echo '</div>';
 // On termine par le pied de page
 
